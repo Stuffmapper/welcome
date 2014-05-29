8c7fea29a2bc5e2c51ffdee32cf606c7b6d56f30
faltantornillos <faltantornillos@gmail.com>
2014-05-28 20:35:35 -0700

    redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
+  get 'redirect/index'
+
   get 'welcome/index'
 
 
   resources :comments, :only => [:new, :create]
 
+  root 'comments#new'
+
+  get '/survey' => 'redirect#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'comments#new'
+  #root 'comments#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
